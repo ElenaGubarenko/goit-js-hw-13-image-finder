25edd6e2fe6a31bb7b2d2d7aa04e57bec72789b5
Elena Gubarenko <lenagritsishina@gmail.com>
2021-03-15 11:18:13 +0300

    adding changes

--- my_project/src/index.js
+++ my_project/src/index.js
@@ -2,28 +2,42 @@ import styles from './styles.css';
 import ImagePage from './js_files/apiService';
 import templateImgCard from './hbs_files/template_img_card.hbs';
 import * as _debounce from 'lodash/debounce';
-// import '~material-icons/css/material-icons.scss';
+// import templateModalImg from './hbs_files/template_modal_img.hbs';
 
 const galleryRef = document.querySelector('.gallery');
 const newImagePage = new ImagePage();
 const inputRef = document.querySelector('.search-form-input');
 const targetRef = document.querySelector('.target');
-let inputRequest = ''.trim();
-const options = {
-  treshold: 0.5,
+const lightboxRef = document.querySelector('.lightbox-content');
+let inputRequest = '';
+
+const fetchData = () => {
+  const newFetch = newImagePage.fetchPhoto(`${inputRequest}`);
+  return newFetch;
 };
 
-const render = () => {
-  const newFetch = newImagePage.fetchPhoto(`${inputRequest}`);
-  newFetch.then(answer => {
-    return galleryRef.insertAdjacentHTML('beforeend', templateImgCard(answer));
+const renderData = () => {
+  return fetchData().then(answer => {
+    galleryRef.insertAdjacentHTML('beforeend', templateImgCard(answer));
   });
+};
+
+const check = () => {
+  inputRequest = inputRef.value;
+  if (inputRequest.trim() === '') {
+    newImagePage.dropPage();
+    galleryRef.innerHTML = '';
+  } else renderData();
+};
+
+const options = {
+  treshold: 0.5,
 };
 
 const callback = (entries, observer) => {
   if (entries[0].isIntersecting) {
     newImagePage.increasePage();
-    render();
+    check();
   }
 };
 
@@ -32,17 +46,23 @@ const obsereverFunc = () => {
   observer.observe(targetRef);
 };
 
-const check = () => {
-  galleryRef.innerHTML = '';
-  inputRequest = inputRef.value;
+const openModal = event => {
+  return fetchData().then(answer => {
+    answer.map(element => {
+      console.log(`fetched ${element.webformatURL}`);
+      console.log(`evented ${event.target.src}`);
+    });
+  });
+  // .then(console.log);
 
-  if (inputRequest.trim() === '') {
-    galleryRef.innerHTML = '';
-  }
-  render();
-  obsereverFunc();
+  // if (event.target.src) {
+  // }
+  // console.dir(event.target);
 };
+
+obsereverFunc();
 
 const debouncedСheck = _debounce(check, 700);
 
 inputRef.addEventListener('input', debouncedСheck);
+galleryRef.addEventListener('click', openModal);
